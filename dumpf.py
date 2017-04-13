86608ce286155fa2cdf545eeca4bcf562a387f86
Nicolas <flaithotw@gmail.com>
2017-04-14 01:03:00 +0200

    Update dumpf.py

--- dumpf.py
+++ dumpf.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+#  For sherwood forest protection check: http://www.hackzapple.com/phpBB2/viewtopic.php?t=894
+#
 """Usage: python dumpf.py [options] file
 
 Options:
@@ -17,8 +20,10 @@ Examples:
   python dumpf.py -d16 <file>
   python dumpf.py --dump=16 <file>
   python dumpf.py --o8192 <file>
-  python dumpf.py --org=$2000 <file>
-  python dumpf.py --org=$2000 -export=file.txt <file>
+  python dumpf.py --org=8129 <file>
+
+  Special for Sherwood Forest ;)
+  python dumpf.py --org=40677 --export=PIC.INDEX#050000.DMP PIC.INDEX#050000
 """
 import sys
 import os
@@ -28,22 +33,28 @@ import codecs
 # To convert bin to ascii/hexa
 import binascii
 
-__author__ = 'Nicolas Djurovic'
+__author__ = 'nico_djurovic'
 
 __VERSION = 0.22
 __SEP = "  "
 __ALL = 1
 
 
+#############################
+# Print usage of the program
 def usage():
     # Will print what is written in the beginning of this file between """
     print(__doc__)
 
 
+#############################
+# Return filesize of file
 def getfilesize(filename):
     return os.stat(filename).st_size
 
 
+#############################
+# Convert byte to hexadecimal
 def byte2hex(data):
     # Given raw bytes, get an ASCII string representing the hex values
     hex_data = binascii.hexlify(data)
@@ -53,13 +64,14 @@ def byte2hex(data):
     return hex_data.decode('utf-8')
 
 
+#############################
+# Convert byte to ascii
 def byte2ascii(data):
     result = ""
     for x in data:
         if x in range(32, 128):  # Check from ascii code $20 to $7E
             result += chr(x)
         elif x in range(160, 255):  # if between $A0 to $FE
-            # x -= 128
             result += chr(x - 128)  # Substract $80 to go back between $20 to $7E
         else:
             result += '.'
@@ -67,15 +79,19 @@ def byte2ascii(data):
     return result
 
 
+#############################
+# Convert bytes to integer
 def byte2int(data):
     return int.from_bytes(data, byteorder='little', signed=False)
 
 
-# Split a line at a fixed size
+#############################
+#  Split a line at a fixed size
 def linewrap(data, size):
     return [data[index:index + size] for index in range(0, len(data), size)]
 
 
+#############################
 # Insert a separator in the hexa line
 # Original is 80AAA28080A0
 # returned: 80 AA A2 80 80 A0
@@ -83,7 +99,10 @@ def insertseparator(string, sep=" "):
     return sep.join(odd + even for odd, even in zip(string[::2], string[1::2])).rstrip()
 
 
+#############################
+# Dump function
 def dump(filename, **kwargs):
+    _org = False
     _export = False
 
     if not filename:
@@ -104,7 +123,8 @@ def dump(filename, **kwargs):
                 _bytes = value
         if key == 'org':
             if value:
-                _org = value
+                _org_address = value
+                _org = True
         if key == 'export':
             if value:
                 _export_file = value
@@ -128,33 +148,46 @@ def dump(filename, **kwargs):
     # Start an index to display the text line
     index = 0
 
+    # To show address of each line
+    if _org is True:
+        origin = int(_org_address)
+
     if _export is True:
         print('Saving file "{}"'.format(_export_file))
-        with open(_export_file, 'w') as f:
-            for each_line in data_hex:
-                # left aligned with a width calculated (option is the number of bytes)
-                # and multiply by 3 because of the space between each byte and minus 1 for the last space
-                chaine = "{:<{width}}{sep}{data}".format(insertseparator(each_line),
-                                                         data=data_txt[index],
-                                                         width=(int(_bytes) * 3) - 1,
-                                                         sep=__SEP)
-                f.write(chaine + '\n')
-                index += 1
-            f.close()
-    else:
-        # Print our result
-        for each_line in data_hex:
-            chaine = "{:<{width}}{sep}{data}".format(insertseparator(each_line),
-                                                     data=data_txt[index],
-                                                     width=(int(_bytes) * 3) - 1,
-                                                     sep=__SEP)
-            # print(codecs.encode(chaine, 'utf-8'))
+        f = open(_export_file, 'w')
+
+    # Loop for each line
+    for each_line in data_hex:
+        chaine = ''
+
+        # If we want to show the address, show it
+        if _org is True:
+            chaine += '{org:>04X}: '.format(org=origin)
+            origin += int(_bytes)
+        # Left aligned with a width calculated and multiplied by 3 because of
+        # the space between each byte and minus 1 for the last space
+        chaine += '{0:<{width}}'.format(insertseparator(each_line), width=(int(_bytes) * 3) - 1)
+        # Then add the seperate string and the text
+        chaine += '{sep}{data}'.format(data=data_txt[index], sep=__SEP)
+
+        # If we export, wrote inside the file
+        if _export is True:
+            f.write(chaine + '\n')
+        else:
             print(chaine)
-            index += 1
+        index += 1
+
+    # and close the file
+    if _export is True:
+        f.close()
+
+
+"""
+That's the main program
+"""
 
 
 def main(argv):
-    # ORGinal address
     _org = 0
     # default value of bytes per line
     _bytes = 8 * 2
@@ -163,6 +196,7 @@ def main(argv):
     # Default filename to export
     _export_file = ''
 
+    # Get arguments
     try:
         opts, args = getopt.getopt(argv, "hd:o:s:vx:", ["help", "dump=", "org=", "show=", "version", "export="])
     except getopt.GetoptError as err:
